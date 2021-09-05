54813ce2710a865a6ae805e1bf5bfa006ff534fc
GlennTJR96 <glenn_tjr@hotmail.com>
2021-09-05 22:54:16 +0800

    removed unnecessary angular components

--- UrlShortening_FE/src/app/app.module.ts
+++ UrlShortening_FE/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UrlComponent } from './models/url/url.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UrlComponent
   ],
   imports: [
     BrowserModule,
