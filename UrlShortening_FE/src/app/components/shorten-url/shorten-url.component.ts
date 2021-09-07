9745470486090251587db477a54c75f6e216b949
GlennTJR96 <glenn_tjr@hotmail.com>
2021-09-07 23:47:05 +0800

    (FE) update shorten url component to validate user input (required and valid url format)

--- UrlShortening_FE/src/app/components/shorten-url/shorten-url.component.ts
+++ UrlShortening_FE/src/app/components/shorten-url/shorten-url.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { UrlService } from 'src/app/services/url.service';
 
 @Component({
@@ -12,22 +12,36 @@ export class ShortenUrlComponent implements OnInit {
   hasResult: boolean = false;
   shortenedUrl: string = '';
 
+  urlRegEx = "^(https|ftp|http|ftps):\\/\\/([a-z\\d_]+\\.)?(([a-zA-Z\\d_]+)(\\.[a-zA-Z]{2,6}))(\\/[a-zA-Z\\d_\\%\\-=\\+]+)*(\\?)?([a-zA-Z\\d=_\\+\\%\\-&\\{\\}\\:]+)?";
+
+
   urlForm: FormGroup = this.formBuilder.group(
-    {suppliedUrlFormControlName: new FormControl('')}
+    {
+      suppliedUrlFormControl: new FormControl('', [
+        Validators.required,
+        Validators.pattern(this.urlRegEx)
+      ])
+    }
   )
 
   constructor(private formBuilder: FormBuilder,
     private urlService: UrlService) { }
 
   ngOnInit(): void {
-    console.log('inisssted!');
   }
 
-  doShorten(): void{
-    const input = this.urlForm.get('suppliedUrlFormControlName').value;
-    this.urlService.saveNewUrl(input).subscribe(res => {
-      this.hasResult = true;
-      this.shortenedUrl = location.origin + '/' + res.hashed;
-    });
+  doShorten(): void {
+    // I should check if input starts with http:// or https://
+    const suppliedUrlFormControl = this.urlForm.get('suppliedUrlFormControl');
+    console.log(suppliedUrlFormControl);
+    console.log(this.urlForm);
+
+    if (suppliedUrlFormControl && this.urlForm.valid) {
+      const input = suppliedUrlFormControl.value;
+      this.urlService.saveNewUrl(input).subscribe(res => {
+        this.hasResult = true;
+        this.shortenedUrl = location.origin + '/' + res.hashed;
+      });
+    }
   }
 }
